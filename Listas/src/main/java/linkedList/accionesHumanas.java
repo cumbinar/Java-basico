4c7bfc14fb2b396491665bda6abb0f6ebe7bde3c
cumbinar <cumbiweb@gmail.com>
2022-07-09 12:25:51 -0500

    refactorize class accionesHumanas

---
+++ Listas/src/main/java/linkedList/accionesHumanas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package linkedList;
+
+/**
+ *
+ * @author educu
+ */
+public interface accionesHumanas {
+    public void trabajar();
+    public void estudiar();
+    
+}
