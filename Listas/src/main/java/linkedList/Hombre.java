9ef562c3aedcc27d4b700cb43d0f2409231b751c
cumbinar <cumbiweb@gmail.com>
2022-07-09 12:25:07 -0500

    refactorize class accionesHumanas

--- Listas/src/main/java/linkedList/Hombre.java
+++ Listas/src/main/java/linkedList/Hombre.java
@@ -8,6 +8,32 @@ package linkedList;
  *
  * @author educu
  */
-public class Hombre {
+public class Hombre extends Humano implements accionesHumanas{
+   
+    private String nombre;
+
+    public Hombre(String nombre, String cedula) {
+        super(cedula);
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    @Override
+    public void trabajar(){
+        System.out.println("El Hombre está trabajando");
+    }
+    
+    @Override
+    public void estudiar(){
+        System.out.println("El Hombre está estudiando");
+    }
+    
     
 }
