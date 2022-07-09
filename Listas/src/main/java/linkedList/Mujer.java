4fc56c0a6403092bc16ed4bbba7c414076ea8ea8
cumbinar <cumbiweb@gmail.com>
2022-07-09 12:21:52 -0500

    refactorize class Mujer

--- Listas/src/main/java/linkedList/Mujer.java
+++ Listas/src/main/java/linkedList/Mujer.java
@@ -3,11 +3,58 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package linkedList;
-
+import java.util.LinkedList;
 /**
  *
  * @author educu
  */
-public class Mujer {
+public class Mujer extends Humano implements accionesHumanas{
+    
+    private String nombre;
+    private LinkedList<String> cursos;
+
+    public Mujer(String nombre, String cedula) {
+        super(cedula);
+        this.nombre = nombre;
+        cursos = new LinkedList();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LinkedList<String> getCursos() {
+        return cursos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCursos(LinkedList<String> cursos) {
+        this.cursos = cursos;
+    }
+    
+    public void agregarCursos(String curso){
+        cursos.add(curso);
+    }
+    
+    public void mostrarCursos(){
+        System.out.println("Los Cursos son");
+        
+        for(String curso:cursos){
+            System.out.println(curso);
+        }
+    }
+    @Override
+    public void trabajar(){
+        System.out.println("La Mujer está trabajando");
+    }
+    
+    @Override
+    public void estudiar(){
+        System.out.println("La Mujer está estudiando");
+    }
+    
     
 }
