d0e08f4754072870230c8540c595d3393ed6233d
cumbinar <cumbiweb@gmail.com>
2022-07-11 19:47:45 -0500

    refactorize proyect Basic

--- Conjuntos/src/main/java/com/mycompany/conjuntos/Conjuntos.java
+++ Conjuntos/src/main/java/com/mycompany/conjuntos/Conjuntos.java
@@ -11,7 +11,8 @@ import java.util.HashSet;
  * @author educu
  */
 public class Conjuntos {
-
+    // En los conjuntos no se repiten objetos
+    //los elementos no tienen orden y el conjunto no se puede indexar
     public static void main(String[] args) {
         HashSet<Integer> conjuntoNumeros = new HashSet<Integer>();  // se crea el conjunto
         conjuntoNumeros.add (20);
@@ -19,9 +20,16 @@ public class Conjuntos {
         conjuntoNumeros.add (30);
         conjuntoNumeros.add (20); // este no lo agrega ya esta repetido
         conjuntoNumeros.add (90);
+        conjuntoNumeros.add (120);
+        conjuntoNumeros.add (90); // este no lo agrega ya esta repetido
         
-        System.out.println(conjuntoNumeros);
+        System.out.println(conjuntoNumeros);  //no se imprime en el orden de igreso
+        System.out.println(conjuntoNumeros);  //no se imprime en el orden de igreso
+        System.out.println(conjuntoNumeros);  //no se imprime en el orden de igreso
         
+          for (int elemento : conjuntoNumeros) {  //forma de recorrer el conjunto
+            System.out.println("Este es numero " + elemento); //no se imprime en el orden de ingreso
+        }
         
         
     }
