6ef07fed1b3f78c75eaad89a3c3bc6d01f219236
cumbinar <cumbiweb@gmail.com>
2022-07-14 09:00:52 -0500

    refactorize Etiqueta class

--- Interfaz/src/main/java/com/mycompany/interfaz/Etiqueta.java
+++ Interfaz/src/main/java/com/mycompany/interfaz/Etiqueta.java
@@ -4,15 +4,53 @@
  */
 package com.mycompany.interfaz;
 
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.*;
+
 /**
  *
  * @author educu
  */
 public class Etiqueta {
+    private int x;
+    private int y;
+    private String titulo;
+
+    public Etiqueta(int x, int y, String titulo) {
+        this.x = x;
+        this.y = y;
+        this.titulo = titulo;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
     
 }
 
 
  
+
    
         
